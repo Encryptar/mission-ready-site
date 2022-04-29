180e7c8861d153a660038ee867b95af97b8ac5ec
Encryptar <64878818+Encryptar@users.noreply.github.com>
2022-04-29 18:47:23 +1200

    Changed chatbot styles

--- src/App.js
+++ src/App.js
@@ -9,9 +9,7 @@ function App() {
         <div className="app">
             <Navbar />
             <Searchbar />
-            <iframe className='chatbot' width="350" height="430" allow="microphone;" src="https://console.dialogflow.com/api-client/demo/embedded/4c1bf653-30e8-4d24-9d76-41b6bd79ac51"></iframe>
             <ImageList />
-            
         </div>
     );
 }
